1266423c0620ce474cbaefc00aca5276c4a66b85
tinysakura <2796191719@qq.com>
2018-09-11 13:58:37 +0800

    测试了rabbitmq的对象序列化机制与几种不同的路由

---
+++ src/main/java/com/cfh/rabbitmq/rabbit/FanoutRabbitConfig.java
@@ -0,0 +1,50 @@
+package com.cfh.rabbitmq.rabbit;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class FanoutRabbitConfig {
+
+    @Bean
+    public Queue AMessage() {
+        return new Queue("fanout.A");
+    }
+
+    @Bean
+    public Queue BMessage() {
+        return new Queue("fanout.B");
+    }
+
+    @Bean
+    public Queue CMessage() {
+        return new Queue("fanout.C");
+    }
+
+    @Bean
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange("fanoutExchange");
+    }
+
+    //使用fanout类型的Exchange绑定了多个queue,所有队列都会接收到被该Exchange路由的所有消息
+    @Bean
+    Binding bindingExchangeA(Queue AMessage,FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(AMessage).to(fanoutExchange);
+    }
+
+    @Bean
+    Binding bindingExchangeB(Queue BMessage, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(BMessage).to(fanoutExchange);
+    }
+
+    @Bean
+    Binding bindingExchangeC(Queue CMessage, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(CMessage).to(fanoutExchange);
+    }
+
+}
---
+++ src/main/java/com/cfh/rabbitmq/rabbit/topic/TopicSender.java
@@ -0,0 +1,33 @@
+package com.cfh.rabbitmq.rabbit.topic;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicSender {
+
+	@Autowired
+	private AmqpTemplate rabbitTemplate;
+
+	//使用指定的Exchange路由消息，选择键指定为topic1，这样只有绑定键为topic.#的队列可以接收到消息
+	public void send() {
+		String context = "hi, i am message all";
+		System.out.println("Sender : " + context);
+		this.rabbitTemplate.convertAndSend("topicExchange", "topic.1", context);
+	}
+
+	//选择键为topic.message,这样绑定键为topic.message与topic.#的队列都可以接受到消息
+	public void send1() {
+		String context = "hi, i am message 1";
+		System.out.println("Sender : " + context);
+		this.rabbitTemplate.convertAndSend("topicExchange", "topic.message", context);
+	}
+
+	public void send2() {
+		String context = "hi, i am messages 2";
+		System.out.println("Sender : " + context);
+		this.rabbitTemplate.convertAndSend("topicExchange", "topic.messages", context);
+	}
+
+}
